83fc23ba73a4906ff1d0cbe2d1386d7ab33edf25
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-26 23:25:48 -0800

    Change icon

--- src/icons.ts
+++ src/icons.ts
@@ -1,6 +1,6 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 
-import testViewerSvg from '../style/icons/video-camera-svgrepo-com.svg';
+import testViewerSvg from '../style/icons/video-player-svgrepo-com.svg';
 
 export const testViewerIcon = new LabIcon({
   name: 'webds_test_viewer_icon',
